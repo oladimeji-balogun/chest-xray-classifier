ef770ef08769cf541d45bf6c5feb2b2a9480035f
Oladimeji <boladimeji834@gmail.com>
2026-03-31 19:44:22 +0100

    finished the building blocks

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -5,7 +5,10 @@ from src.models.numpy_net import (
     max_pool2d, 
     softmax, 
     flatten, 
-    cross_entropy_loss
+    cross_entropy_loss, 
+    fc_forward, 
+    fc_backward,
+    relu_backward
 )
 import numpy as np 
 
@@ -40,3 +43,35 @@ class TestSoftmax:
         x = np.random.randn(5)
         softmax_x = softmax(x)
         assert np.isclose(np.sum(softmax_x), 1.0)
+
+class TestFCLayer:
+    def test_fc_forward_shape(self): 
+        W = np.random.randn(4, 8)
+        x = np.random.randn(8)
+        b = np.random.randn(4)
+        output = fc_forward(W=W, x=x, b=b)
+        assert output.shape == (4,)
+
+    def test_fc_backward_shapes(self): 
+        dout = np.random.randn(4)
+        x = np.random.randn(8)
+        W = np.random.randn(4, 8)
+        b = np.random.randn(4)
+
+        dL_dW, dL_db, dL_dx = fc_backward(dout=dout, x=x, W=W)
+        assert dL_dW.shape == W.shape
+        assert dL_dx.shape == x.shape
+        assert dL_db.shape == b.shape
+
+class TestReLUBackwards: 
+    def test_block_negative_gradients(self): 
+        x = np.array([1, 2, -3, -4, 5])
+        dout = np.array([1, 1, 1, 1, 1])
+        gradient = relu_backward(dout=dout, x=x)
+        assert np.allclose(gradient, np.array([1, 1, 0, 0, 1]))
+
+    def test_scale_positive_gradients(self): 
+        x = np.array([4, 5, -2, 3])
+        dout = np.array([3, 3, 3, 3])
+        gradient = relu_backward(dout=dout, x=x)
+        assert np.allclose(gradient, np.array([3, 3, 0, 3]))
